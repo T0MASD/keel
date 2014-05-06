14b70cd3ff42acfea7d87ec95761ef123468503b
Tomas Dabasinskas <tdabasin@iddqd.bne.redhat.com>
2014-05-06 16:10:01 +1000

    rename functions

--- keel/views/projects.py
+++ keel/views/projects.py
@@ -11,14 +11,14 @@ def list_projects(context, request):
 
 
 @view_config(request_method='POST', context=Projects, renderer='json')
-def create_city(context, request):
+def create_project(context, request):
     result = context.create(request.json_body)
 
     return request.json_body
 
 
 @view_config(request_method='GET', context=Project, renderer='json')
-def get_city(context, request):
+def get_project(context, request):
     r = context.retrieve()
 
     if r is None:
