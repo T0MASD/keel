c4fa1a25f3483d7e31eb31eb7146dbed41edc538
Tomas Dabasinskas <tdabasin@iddqd.bne.redhat.com>
2014-05-07 10:23:10 +1000

    add response to OPTIONS method called by ajax rest client

--- keel/views/__init__.py
+++ keel/views/__init__.py
@@ -4,3 +4,10 @@ from pyramid.view import view_config
 @view_config(renderer='json', context=Root)
 def home(context, request):
     return {'info': 'Project API'}
+
+
+@view_config(request_method='OPTIONS')
+def handle_options(request):
+    ''' this is dummy reponse to OPTIONS method called by ajax, 
+    request.response.headers are set in add_cors_headers_response_callback '''
+    return request.response
