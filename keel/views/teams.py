e2177d3b6165ae5243c4ea7c9027d2b07c995bf7
Tomas Dabasinskas <tdabasin@iddqd.bne.redhat.com>
2014-05-19 11:58:50 +1000

    abandon code introduced in commit 64a3499c0b241e549a75d38038be31b6e986a607

--- keel/views/teams.py
+++ keel/views/teams.py
@@ -1,4 +1,4 @@
-from keel.resources import Teams, Team, Members
+from keel.resources import Teams, Team
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPNotFound
 from pyramid.response import Response
@@ -7,18 +7,8 @@ from pyramid.response import Response
 @view_config(request_method='GET', context=Teams, renderer='json')
 def list_project_teams(context, request):
     parent_project_id = context.__parent__.__name__
-    teams = context.retrieve(spec={"project_id":parent_project_id}, fields={"name":1})
-    # add members to the teams
-    for team in teams:
-        # strip ObjectId()
-        team_id = str(team['_id'])
-        # initialize members collection
-        members = Members(ref='', parent=None)
-        # add request required for db comms
-        members.request = request
-        # set members property
-        team['members'] = members.retrieve(spec={"team_id":team_id})
-    return teams
+    r = context.retrieve(spec={"project_id":parent_project_id}, fields={"name":1})
+    return r
 
 
 @view_config(request_method='GET', context=Team, renderer='json')
