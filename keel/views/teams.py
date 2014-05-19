457653004e8fe0259bee611cf40ba20341884928
Tomas Dabasinskas <tdabasin@iddqd.bne.redhat.com>
2014-05-19 15:47:06 +1000

    add create, update, delete for team

--- keel/views/teams.py
+++ keel/views/teams.py
@@ -24,3 +24,31 @@ def get_project_team(context, request):
             return r
         else:
             raise HTTPNotFound()
+
+
+@view_config(request_method='POST', context=Teams, renderer='json')
+def create_team(context, request):
+    # parse json
+    json_body = request.json_body
+    # create team, returns Team objext
+    team = context.create(json_body)
+    # update json body to include team id
+    json_body.update(team.spec)
+    # return updated json body
+    return json_body
+    
+
+@view_config(request_method='PATCH', context=Team, renderer='json')
+@view_config(request_method='PUT', context=Team, renderer='json')
+def update_team(context, request):
+    context.update(request.json_body, True)
+    json_body = {'name': request.json_body['name']}
+
+    return Response(status_int=202, json_body=json_body)
+
+
+@view_config(request_method='DELETE', context=Team, renderer='json')
+def delete_team(context, request):
+    context.delete()
+    
+    return Response(status_int=202)    
