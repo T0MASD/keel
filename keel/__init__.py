9ec7e216d7d3e553f9ffb3c8a3eb03c24bdbccd9
Tomas Dabasinskas <tdabasin@iddqd.bne.redhat.com>
2014-05-23 09:29:13 +1000

    expose Toaster-Notification and CSRF-Token headers

--- keel/__init__.py
+++ keel/__init__.py
@@ -40,7 +40,7 @@ def add_cors_headers_response_callback(event):
         'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
         'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Max-Age': '1728000',
-        'Access-Control-Expose-Headers': 'Content-Length'
+        'Access-Control-Expose-Headers': 'X-Toaster-Notification, X-CSRF-Token',
         })
     event.request.add_response_callback(cors_headers)
 
