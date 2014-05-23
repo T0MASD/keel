dde454e8e2f96a38370c844a0ce060b1c2988f4a
Tomas Dabasinskas <tdabasin@iddqd.bne.redhat.com>
2014-05-23 10:07:39 +1000

    add basic auth

--- keel/tests/test_views_auth.py
+++ keel/tests/test_views_auth.py
@@ -13,6 +13,8 @@ class UnitTests(unittest.TestCase):
     def test_login_view(self):
         from ..views.auth import login
         request = testing.DummyRequest()
+        # add auth info, paste.httpheaders.AUTHORIZATION also wants to know wsgi.version
+        request.environ.update({'HTTP_AUTHORIZATION': 'Basic bWFuYWdlcnVzZXI6', 'wsgi.version': (1, 0)})
         response = login(request)
         self.assertEqual(response['username'], 'manageruser')
 
@@ -46,12 +48,14 @@ class IntegrationTests(unittest.TestCase):
 
 
     def test_login_view(self):
+        self.app.authorization = ('Basic', ('user', 'password'))
         response = self.app.get('/login')
         self.assertEqual(response.status_int, 200)
 
 
     def test_logout_view(self):
         # run login to start session
+        self.app.authorization = ('Basic', ('user', 'password'))
         self.app.get('/login')
         response = self.app.get('/logout')
         self.assertEqual(response.status_int, 200)
--- keel/views/auth.py
+++ keel/views/auth.py
@@ -1,20 +1,52 @@
 from pyramid.security import remember, forget
 from pyramid.view import view_config
 from pyramid.session import check_csrf_token
+from paste.httpheaders import AUTHORIZATION
+import pyramid.httpexceptions as exc
+
+
+def get_basicauth_credentials(request):
+    authorization = AUTHORIZATION(request.environ)
+    try:
+        authmeth, auth = authorization.split(' ', 1)
+    except ValueError:  # not enough values to unpack
+        return None
+    if authmeth.lower() == 'basic':
+        try:
+            auth = auth.strip().decode('base64')
+        except binascii.Error:  # can't decode
+            return None
+        try:
+            username, password = auth.split(':', 1)
+        except ValueError:  # not enough values to unpack
+            return None
+        return {'username': username, 'password': password}
+
+    return None
+
+
+def check_credentials(credentials):
+    if credentials != None and set(['username', 'password']) == set(credentials.keys()):
+        # DO Login check here
+        return credentials['username']
 
 
 @view_config(route_name='login', renderer='json')
 def login(request):
     """ login view """
     if not request.authenticated_userid:
-        username = 'manageruser'
-        headers = remember(request, username)
-        request.response.headerlist.extend(headers)
-        request.response.headers['X-CSRF-Token'] = request.session.new_csrf_token().encode('utf-8')
-        csrf_token = request.response.headers['X-CSRF-Token']
+        credentials = get_basicauth_credentials(request)
+        username = check_credentials(credentials)
+        if username:
+            headers = remember(request, username)
+            request.response.headerlist.extend(headers)
+            return {'username':username}
+        else:
+            raise exc.HTTPUnauthorized
     else:
         username = request.authenticated_userid
-    return {'username':username}
+        return {'username':username}
+    
 
 
 @view_config(route_name='logout', permission='authenticated', renderer='json')
